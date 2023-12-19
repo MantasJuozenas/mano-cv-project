47a6178b54e1f3c4d57156be3bceab833966c381
Mantas <m.juozenas@gmail.com>
2023-12-19 19:35:15 +0200

    removed wrapper and global styles from app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { GlobalStyle } from './components/SharedStyles.styled';
-import { StyledWrapper } from './components/Wrapper/Wrapper.styled';
 import { MainCard } from './components/MainCard/MainCard';
 
 function App() {
   return (
-    <StyledWrapper>
-      <GlobalStyle />
+    <>
       <MainCard />
-    </StyledWrapper>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,33 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { StyledWrapper } from './components/Wrapper/Wrapper.styled.tsx';
+import { GlobalStyle } from './components/SharedStyles.styled.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
   },
+  {
+    path: '/experience',
+    element: <App />,
+  },
+  {
+    path: '/aboutme',
+    element: <App />,
+  },
+  {
+    path: '/education',
+    element: <App />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <StyledWrapper>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </StyledWrapper>
   </StrictMode>
 );
